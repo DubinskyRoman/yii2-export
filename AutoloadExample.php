35d9bc5a29c816ad1dd6088cd405641d4d045ab1
dubinskyroman <dubinsky.roman@gmail.com>
2015-03-19 23:59:50 +0200

    r9

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -2,6 +2,7 @@
 
 namespace rmd\export;
 
+
 use \PHPExcel;
 use \PHPExcel_IOFactory;
 use \PHPExcel_Settings;
@@ -15,15 +16,15 @@ use \PHPExcel_Writer_Excel2007;
  
  
  
-class MultyExportButton extends \yii\base\Widget
+class AutoloadExample extends \yii\base\Widget
 {
-public $message;
+public $exportType;
 
     public function init()
     {
         parent::init();
-        if ($this->message === null) {
-            $this->message = 'Hello World';
+        if ($this->exportType === null) {
+            $this->exportType = '';
         }
     }
 
@@ -31,56 +32,73 @@ public $message;
 
     public function run()
     {
-        return $this->makeExcel();
-		///return $this->render("index");
-		
-    }
+		if($this->exportType=="xlsx"){
+       	  $this->makeExcel(true);
+		}
+		else if($this->exportType=="xls"){
+       	  $this->makeExcel(false);
+		}
+		else if($this->exportType=="html"){
+       	  $this->makeHtml();
+		  }		
+		else {
+			return  $this->render("index");
+		}
+     }
 	
-	public function makeExcel(){
-				 // Create new PHPExcel object
-				//echo date('H:i:s') . " Create new PHPExcel object\n";
+	public function makeHtml(){
+		  header("Content-Type: text/html");
+		  header('Content-Disposition: attachment; filename="file.html"');
+	      while (ob_get_level() > 1) {
+                ob_end_clean();
+            }
+            echo "1";
+      
+          \Yii::$app->end();
+	}
+	
+	public function makeExcel($isxlsx){
 				$objPHPExcel = new PHPExcel();
-
-				// Set properties
-				//echo date('H:i:s') . " Set properties\n";
 				$objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
 				$objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
 				$objPHPExcel->getProperties()->setTitle("Office 2007 XLSX Test Document");
 				$objPHPExcel->getProperties()->setSubject("Office 2007 XLSX Test Document");
 				$objPHPExcel->getProperties()->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.");
-
-
-				// Add some data
-				//echo date('H:i:s') . " Add some data\n";
-				$objPHPExcel->setActiveSheetIndex(0);
+  			    $objPHPExcel->setActiveSheetIndex(0);
 				$objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello');
 				$objPHPExcel->getActiveSheet()->SetCellValue('B2', 'world!');
 				$objPHPExcel->getActiveSheet()->SetCellValue('C1', 'Hello');
 				$objPHPExcel->getActiveSheet()->SetCellValue('D2', 'world!');
+			    $objPHPExcel->getActiveSheet()->setTitle('Simple');
+                ///header("Pragma: public");
+				// header("Expires: 0");
+				// header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+				// header("Content-Type: application/force-download");
+				// header("Content-Type: application/octet-stream");
+				// header("Content-Type: application/download");;
+				// $filename = "test";
+				//  header("Content-Disposition: attachment;filename=".$filename.".xls");
+				// header("Content-Transfer-Encoding: binary ");
+				
+						 if($isxlsx){
+							 $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel); 
+							 //$objWriter->setOffice2003Compatibility(true);
+							 header("Content-Type: application/octet-stream");
+							 header('Content-Disposition: attachment; filename="file.xlsx"');
+						 }
+						 else
+						 {
+							$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+							header('Content-type: application/vnd.ms-excel');
+							header('Content-Disposition: attachment; filename="file.xls"');
+						 }
 
-				// Rename sheet
-				//echo date('H:i:s') . " Rename sheet\n";
-				$objPHPExcel->getActiveSheet()->setTitle('Simple');
-
-						
-				// Save Excel 2007 file
-				//echo date('H:i:s') . " Write to Excel2007 format\n";
-				//$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
-				//$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
-
-				// Echo done
-				//echo date('H:i:s') . " Done writing file.\r\n";
-				header("Pragma: public");
-				header("Expires: 0");
-				header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-				header("Content-Type: application/force-download");
-				header("Content-Type: application/octet-stream");
-				header("Content-Type: application/download");;
-				header("Content-Disposition: attachment;filename=$filename.xls");
-				header("Content-Transfer-Encoding: binary ");
-				$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel); 
-				$objWriter->setOffice2003Compatibility(true);
-				$objWriter->save('php://output');
+		    while (ob_get_level() > 1) {
+                ob_end_clean();
+            }
+            
+           $objWriter->save('php://output');
+            \Yii::$app->end();
 	}
 	
 	
--- views/index.php
+++ views/index.php
@@ -1,11 +1,15 @@
+<?php
+ use  yii\helpers\Html; 
+ use  yii\helpers\Url;
+ ?>
 <div class="btn-group">
   <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
     Save as... <span class="caret"></span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <li><a href="#">HTML</a></li>
-    <li><a href="#">PDF</a></li>
-    <li><a href="#">XLS</a></li>
-    <li><a href="#">XLSX</a></li>	
+    <li><?= Html::a('HTML', [Url::base(), 'rmdExportType' => 'html']); ?></li>
+    <li><?= Html::a('PDF', [Url::base(), 'rmdExportType' => 'pdf']); ?></li>
+    <li><?= Html::a('XLS', [Url::base(), 'rmdExportType' => 'xls']); ?></li>
+    <li><?= Html::a('XLSX', [Url::base(), 'rmdExportType' => 'xlsx']); ?></li>	
   </ul>
 </div>
