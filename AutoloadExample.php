dd42b1be9bfd92c68540f5e1cc361da34de9427f
dubinskyroman <dubinsky.roman@gmail.com>
2015-03-19 02:18:07 +0200

    0.0.0.3

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -2,6 +2,12 @@
 
 namespace rmd\export;
 
+use \PHPExcel;
+use \PHPExcel_IOFactory;
+use \PHPExcel_Settings;
+use \PHPExcel_Style_Fill;
+use \PHPExcel_Writer_IWriter;
+use \PHPExcel_Worksheet;
 /**
  * This is just an example.
  */
@@ -10,5 +16,45 @@ class AutoloadExample extends \yii\base\Widget
     public function run()
     {
         return "Hello!";
+		makeExcel();
     }
+	
+	public makeExcel(){
+				// Create new PHPExcel object
+				echo date('H:i:s') . " Create new PHPExcel object\n";
+				$objPHPExcel = new PHPExcel();
+
+				// Set properties
+				echo date('H:i:s') . " Set properties\n";
+				$objPHPExcel->getProperties()->setCreator("Maarten Balliauw");
+				$objPHPExcel->getProperties()->setLastModifiedBy("Maarten Balliauw");
+				$objPHPExcel->getProperties()->setTitle("Office 2007 XLSX Test Document");
+				$objPHPExcel->getProperties()->setSubject("Office 2007 XLSX Test Document");
+				$objPHPExcel->getProperties()->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.");
+
+
+				// Add some data
+				echo date('H:i:s') . " Add some data\n";
+				$objPHPExcel->setActiveSheetIndex(0);
+				$objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello');
+				$objPHPExcel->getActiveSheet()->SetCellValue('B2', 'world!');
+				$objPHPExcel->getActiveSheet()->SetCellValue('C1', 'Hello');
+				$objPHPExcel->getActiveSheet()->SetCellValue('D2', 'world!');
+
+				// Rename sheet
+				echo date('H:i:s') . " Rename sheet\n";
+				$objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+						
+				// Save Excel 2007 file
+				echo date('H:i:s') . " Write to Excel2007 format\n";
+				$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+				$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+
+				// Echo done
+				echo date('H:i:s') . " Done writing file.\r\n";
+	}
+	
+	
+	
 }
